fbb53a0b2e99ff040a4ae5b53fe29f1b446395f7
rafaUTVT <al222111269@gmail.com>
2022-07-13 22:59:10 -0500

    Conexión con BD establecida

--- PHP/registro.php
+++ PHP/registro.php
@@ -1,9 +1,9 @@
 <?php 
 
-include("./con_db.php");
+include("con_db.php");
 
 if (isset($_POST['subir'])) {
-    if (strlen($_POST['Nombre']) >= 1 && strlen($_POST['Apellido_a']) >= 1 && strlen($_POST['Apellido_b']) >= 1 && strlen($_POST['Telefono']) >= 1 && strlen($_POST['Email']) >= 1 && strlen($_POST['Empresa']) >= 1 && strlen($_POST['Lista']) >= 1 /* && strlen($_POST['Empresa']) >= 1 */) {
+    if (strlen($_POST['Nombre']) >= 1 && strlen($_POST['Apellido_a']) >= 1 && strlen($_POST['Apellido_b']) >= 1 && strlen($_POST['Telefono']) >= 1 && strlen($_POST['Email']) >= 1 && strlen($_POST['Empresa']) >= 1 && strlen($_POST['Lista']) >= 1 && strlen($_POST['Empresa']) >= 1 ) {
 
 	    $nombre = trim($_POST['Nombre']);
 	    $ape_a = trim($_POST['Apellido_a']);
@@ -12,11 +12,11 @@ if (isset($_POST['subir'])) {
 		$email = trim($_POST['Email']);
 		$empresa = trim($_POST['Empresa']);
 		$servicio = trim($_POST['Lista']);
-		//$descri = trim($_POST['Area']);
-		//$descri = trim($_POST['Area']);
+		$descri = trim($_POST['Area']);
 
 
-	    $consulta = "INSERT INTO usuarios(Nombre, Primer_A, Segundo_A, Telefono, Correo, Nom_empresa, Servicio, /*Mensaje*/) VALUES ('$nombre','$ape_a','$ape_b','$telefo','$email','$empresa','$servicio',/*'$descri'*/)";
+
+	    $consulta = "INSERT INTO usuarios(Nombre, Primer_A, Segundo_A, Telefono, Correo, Nom_empresa, Servicio, Mensaje) VALUES ('$nombre','$ape_a','$ape_b','$telefo','$email','$empresa','$servicio','$descri')";
 
 
 	    $resultado = mysqli_query($conex,$consulta);
