4d150a6456f2a568736c2fd0d02cbbd8c0cb2713
TerexWeb <jesusLGutierrez685@gmail.com>
2022-08-04 03:02:43 -0500

    modificacion del menu

--- config/parameters.php
+++ config/parameters.php
@@ -5,10 +5,7 @@ define ("base_url_admin","http://localhost/DayCode.MX/admin");
 define ("KEY_TOKEN", "APR.wqc-354*");
 define ("MONEDA", "$" );
 //  session_start();
-$num_cart = 0;
-if (isset ($_SESSION['carrito'][' productos '])) {
-    $num_cart = count($_SESSION['carrito'][' productos ']);
-}
+
 
 
 
