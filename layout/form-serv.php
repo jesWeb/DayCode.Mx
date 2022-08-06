221567ea9330fb07da38c004ca09ba4ee6c53020
rafaUTVT <al222111269@gmail.com>
2022-08-05 22:44:10 -0500

    Asignacion H-F 2/6

--- layout/form-serv.php
+++ layout/form-serv.php
@@ -1,66 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <link rel="stylesheet" href="./css/bootstrap.min.css" />
-</head>
+session_start();
+     $pagina_admin = 2;     
+     $pagina_modificacion= 0;
+     $nombre_pagina = "Home";
+     $pagina =0;
+     $modal=0;
 
-<body>
-  <div class="container ">
-    <div class="row bg-form">
-      <div class="col-6">
-        <!-- form -->
-        <div class="box-form">
-          <h1>¿Quien nos escribe?</h1>
+require_once '../includes/header.php';
 
-          <form action="" class="">
-            <!-- Name -->
-            <input type="text" name="" id="" placeholder="Nombre" />
+?>
 
-            <!-- Ape-parterno -->
-            <input type="text" name="" id="" placeholder="Apellido Paterno" />
+<main>
 
-            <!-- Ape-Materno -->
-            <input type="text" name="" id="" placeholder="Apellido Materno" />
+  <body>
+    <div class="container ">
+      <div class="row bg-form">
+        <div class="col-6">
+          <!-- form -->
+          <div class="box-form">
+            <h1>¿Quien nos escribe?</h1>
 
-            <!-- Telefono -->
-            <input type="text" name="" id="" placeholder="Telefono" />
+            <form action="" class="">
+              <!-- Name -->
+              <input type="text" name="" id="" placeholder="Nombre" />
 
-            <!-- Email -->
-            <input type="email" name="" id="" placeholder="Correo electronico" />
+              <!-- Ape-parterno -->
+              <input type="text" name="" id="" placeholder="Apellido Paterno" />
 
-            <!--Name-empresa-->
-            <input type="text" name="" id="" placeholder="Nombre de tu empresa" />
+              <!-- Ape-Materno -->
+              <input type="text" name="" id="" placeholder="Apellido Materno" />
 
-            <!-- description -->
-            <textarea name="" id="" cols="30" rows="10" placeholder="Compartenos sobre tu Empresa"> </textarea>
-          </form>
-          <!-- btn-form -->
-          <a href="" class="btn btn-primary">Enviar</a>
+              <!-- Telefono -->
+              <input type="text" name="" id="" placeholder="Telefono" />
+
+              <!-- Email -->
+              <input type="email" name="" id="" placeholder="Correo electronico" />
+
+              <!--Name-empresa-->
+              <input type="text" name="" id="" placeholder="Nombre de tu empresa" />
+
+              <!-- description -->
+              <textarea name="" id="" cols="30" rows="10" placeholder="Compartenos sobre tu Empresa"> </textarea>
+            </form>
+            <!-- btn-form -->
+            <a href="" class="btn btn-primary">Enviar</a>
+          </div>
         </div>
-      </div>
-      <!-- description-form -->
-      <div class="col-6 Desc-form">
-        <!-- descripcion -->
-        <div class="bg-danger">
-          <p>
-            “Vemos a nuestros clientes como los invitados de <br />
-            una fiesta en la que nosotros somos los anfitriones. <br />
-            <br />
-            Nuestro trabajo es hacer que cada aspecto importante<br />
-            de la experiencia del consumidor sea un poco mejor”
-          </p>
-          <img src="./IMG/icons/undraw_add_to_cart_re_wrdo.svg" class="svg-form" alt="" />
+        <!-- description-form -->
+        <div class="col-6 Desc-form">
+          <!-- descripcion -->
+          <div class="bg-danger">
+            <p>
+              “Vemos a nuestros clientes como los invitados de <br />
+              una fiesta en la que nosotros somos los anfitriones. <br />
+              <br />
+              Nuestro trabajo es hacer que cada aspecto importante<br />
+              de la experiencia del consumidor sea un poco mejor”
+            </p>
+            <img src="./IMG/icons/undraw_add_to_cart_re_wrdo.svg" class="svg-form" alt="" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</body>
-<!-- Bp-js -->
-<script src="./js/bootstrap.bundle.min.js"></script>
+  </body>
+</main>
 
-</html>
+<?php 
+require_once '../includes/footer.php';
+?>
