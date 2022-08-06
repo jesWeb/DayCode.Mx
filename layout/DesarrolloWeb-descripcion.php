251ab0eaa5152ff516723cd57f9446dee8dc33bb
rafaUTVT <al222111269@gmail.com>
2022-08-05 22:59:04 -0500

    Asignacion H-F 5/6

--- layout/DesarrolloWeb-descripcion.php
+++ layout/DesarrolloWeb-descripcion.php
@@ -13,287 +13,120 @@ require_once '../includes/header.php';
 
 <main>
 
-<body>
-<div class="container">
-        <!-- ROW Presentación -->
+  <body>
+    <!-- Descripcion -->
+    <div class="container">
+      <section class="mt-5">
         <div class="mt-5">
-            <div class="row justify-content-center align-content-center ">
-                <div class="col-md-6">
-                    <h1>DayAcademy</h1>
-                    <div class="daycode-academy_description mt-4 p-auto ">
-                        <p>
-                            Este espacio, es tu lugar para adquirir los conocimientos
-                            básicos para la innovacion de tu negocio y potenciar el alcance
-                            de tu marca.
-                            Tu decides... ¿Qué es lo que quieres aprender?
-                            DayAcademy.
-                        </p>
-                        <p>
-                            “La mente es como un paracaídas: sólo funciona si se abre” <br>
-                            ~ Albert Einstein.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-6 text-center">
-                    <img src="./IMG/Academy.png" alt="" class="img-fluid " style="height: 480px;">
-                </div>
+          <div class="row ">
+            <div class="col-md-6 mt-5 img-course">
+              <img src="./IMG/card-web-1.jpg" alt="" class="img-fluid rounded-3">
             </div>
-        </div>
-        <!-- ´section courses -->
-        <section class="Academy-courses mt-5">
-            <div class=" mt-5">
-                <h3 class="text-center mb-4">El viaje al futuro comienza aqui </h3>
-                <div class="row">
-                    <!-- curso1 -->
-                    <div class="col-md-4 mt-3">
-                        <!-- ´card -->
-                        <div class="card card-academy border-0">
-                            <!-- card-img -->
-                            <img src="https://picsum.photos/500/300" class="card-img-top rounded-2  " alt="">
-                            <!-- card-content -->
-                            <div class="card-body card-academy_content bg-ligth ">
-                                <!-- title-->
-                                <h4 class="card-title text-center">Seo y ADS</h4>
-                                <!-- ´texto -->
-                                <p class="card-text">Precio <span>$4,150</span></p>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-                                    maiores perspiciatis ipsum omnis incidunt veritatis!</p>
-                                <p class="card-text h3">$1200</p>
-                                <div class="mt-5 text-center">
-                                    <!-- ´btn -->
-                                    <a href="#" class="card-link btn btn-primary rounded-pill3">Comprar</a>
-                                    <a href="#" class="card-link btn btn-primary rounded-pill-3">Agregar carrito</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- curso1 -->
-                    <div class="col-md-4 mt-3">
-                        <!-- ´card -->
-                        <div class="card card-academy border-0">
-                            <!-- card-img -->
-                            <img src="https://picsum.photos/500/300" class="card-img-top rounded-2  " alt="">
-                            <!-- card-content -->
-                            <div class="card-body card-academy_content bg-ligth ">
-                                <!-- title-->
-                                <h4 class="card-title text-center">Seo y ADS</h4>
-                                <!-- ´texto -->
-                                <p class="card-text">Precio <span>$4,150</span></p>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-                                    maiores perspiciatis ipsum omnis incidunt veritatis!</p>
-                                <p class="card-text h3">$1200</p>
-                                <div class="mt-5 text-center">
-                                    <!-- ´btn -->
-                                    <a href="#" class="card-link btn btn-primary rounded-pill3">Comprar</a>
-                                    <a href="#" class="card-link btn btn-primary rounded-pill-3">Agregar carrito</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- curso1 -->
-                    <div class="col-md-4 mt-3">
-                        <!-- ´card -->
-                        <div class="card card-academy border-0">
-                            <!-- card-img -->
-                            <img src="https://picsum.photos/500/300" class="card-img-top rounded-2  " alt="">
-                            <!-- card-content -->
-                            <div class="card-body card-academy_content bg-ligth ">
-                                <!-- title-->
-                                <h4 class="card-title text-center">Seo y ADS</h4>
-                                <!-- ´texto -->
-                                <p class="card-text">Precio <span>$4,150</span></p>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-                                    maiores perspiciatis ipsum omnis incidunt veritatis!</p>
-                                <p class="card-text h3">$1200</p>
-                                <div class="mt-5 text-center">
-                                    <!-- ´btn -->
-                                    <a href="#" class="card-link btn btn-primary rounded-pill3">Comprar</a>
-                                    <a href="#" class="card-link btn btn-primary rounded-pill-3">Agregar carrito</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+            <div class="col-md-6 mt-5">
+              <h1>Desarrollo Web</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias dicta, voluptate provident
+                exercitationem aspernatur voluptatibus similique architecto vel quo obcaecati quas perspiciatis nobis
+                libero, explicabo dolorem, expedita error ipsam enim.</p>
+              <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
+              <a href="#" class="btn btn-primary">COMPRAR</a>
 
-    </div>
-</body>
+            </div>
+          </div>
+        </div>
+      </section>
+      </section>
+      <!-- Recomended -->
+      <section class="mt-3 mb-4 bg-primary">
+        <h3 class="h4">Recomendado para ti</h3>
+        <!-- cards -->
+        <section class="mt-3 mb-5">
+          <div class="row">
+            <!-- card 1 -->
+            <div class="col-sm-4 mt-md-4">
+              <a href="" class="text-decoration-none">
+                <div class="card">
+                  <!-- img  card-->
+                  <div class="card-Rc">
+                    <img src="./IMG/card-programa-1.jpg" class="card-Rc_img img-fluid" alt="">
+                  </div>
+                  <!-- description -->
+                  <div class="card-body">
+                    <!-- title -->
+                    <h4 class="card-title h5 text-dark">
+                      JAVA
+                    </h4>
+                    <!-- descr -->
+                    <p class="card-text_p">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit dolorem natus.
+                      <br>
+                      <span class="card-precio">$11,000</span>
+                    </p>
+                    <!-- precio -->
+                    <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <!-- card -->
+            <div class="col-md-4 mt-md-4">
+              <a href="" class="text-decoration-none">
+                <div class="card">
+                  <!-- img  card-->
+                  <div class="card-Rc">
+                    <img src="./IMG/card-maqueta-2.jpg" class="card-Rc_img img-fluid" alt="">
+                  </div>
+                  <!-- description -->
+                  <div class="card-body">
+                    <!-- title -->
+                    <h4 class="card-title h5 text-dark">
+                      Desarrollo Web
+                    </h4>
+                    <!-- descr -->
+                    <p class="card-text_p">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit dolorem
+                      natus.
+                      <br>
+                      <span class="card-precio">$11,000</span>
+                    </p>
+                    <!-- precio -->
+                    <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <!-- card -->
+            <div class="col-md-4 mt-md-4">
+              <a href="" class="text-decoration-none">
+                <div class="card">
+                  <!-- img  card-->
+                  <div class="card-Rc">
+                    <img src="./IMG/card-programa-2.jpg" class="card-Rc_img img-fluid" alt="">
+                  </div>
+                  <!-- description -->
+                  <div class="card-body">
+                    <!-- title -->
+                    <h4 class="card-title h5 text-dark">
+                      Base de Datos
+                    </h4>
+                    <!-- descr -->
+                    <p class="card-text_p">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit dolorem
+                      natus.
+                      <br>
+                      <span class="card-precio">$11,000</span>
+                    </p>
+                    <!-- precio -->
+                    <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </section>
+      </section>
+  </body>
 </main>
 
 <?php 
 require_once '../includes/footer.php';
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="./css/courses.css">
-</head>
-<!-- Menu-->
-<header class="">
-    <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow navbar-main ">
-      <div class="container-fluid">
-        <!-- branding -->
-        <a href="" class="navbar-brand ">
-          <img src="./IMG/DAYCODE-nav.png" alt="brand DAYCODE.mx" class=" ms-md-0">
-        </a>
-        <!-- btn -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
-          aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- items -->
-        <div class="collapse navbar-collapse" id="mainNav">
-          <div class="nav ms-auto ">
-            <!-- links -->
-            <a href="index.html" class="nav-link nav-link_n active m-2">Home</a>
-            <a href="./services.html" class="nav-link nav-link_n m-2">Servicios</a>
-            <a href="./Academy.html" class="nav-link nav-link_n m-2">Academy</a>
-            <a href="" class="nav-link nav-link_n m-2">Contacto</a>
-            <button class="nav-link btn  m-2 d-grid-md-2 mt-md-2 nav-link_btn">Iniciar sesion</button>
-            <button class="nav-link btn  m-2 d-grid-md-2 mt-md-2 nav-link_btn-up">Crear cuenta</button>
-          </div>
-    </nav>
-  </header>
-<hr>
-
-<body>
-    <!-- Descripcion -->
-    <div class="container">
-      <section class="mt-5">
-          <div class="mt-5">
-              <div class="row ">
-                  <div class="col-md-6 mt-5 img-course">
-                      <img src="./IMG/card-web-1.jpg" alt="" class="img-fluid rounded-3">
-                  </div>
-                  <div class="col-md-6 mt-5">
-                      <h1>Desarrollo Web</h1>
-                      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias dicta, voluptate provident exercitationem aspernatur voluptatibus similique architecto vel quo obcaecati quas perspiciatis nobis libero, explicabo dolorem, expedita error ipsam enim.</p>
-                      <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
-                      <a href="#" class="btn btn-primary">COMPRAR</a>
-                      
-                  </div>
-              </div>
-          </div>
-      </section>
-  </section>
-     <!-- Recomended -->
-     <section class="mt-3 mb-4 bg-primary">
-      <h3 class="h4">Recomendado para ti</h3>
-      <!-- cards -->
-      <section class="mt-3 mb-5">
-        <div class="row">
-          <!-- card 1 -->
-          <div class="col-sm-4 mt-md-4">
-            <a href="" class="text-decoration-none">
-              <div class="card">
-                <!-- img  card-->
-                <div class="card-Rc">
-                  <img src="./IMG/card-programa-1.jpg" class="card-Rc_img img-fluid" alt="">
-                </div>
-                <!-- description -->
-                <div class="card-body">
-                  <!-- title -->
-                  <h4 class="card-title h5 text-dark">
-                    JAVA
-                  </h4>
-                  <!-- descr -->
-                  <p class="card-text_p">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit dolorem natus.
-                    <br>
-                    <span class="card-precio">$11,000</span>
-                  </p>
-                  <!-- precio -->
-                  <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
-                </div>
-              </div>
-            </a>
-          </div>
-          <!-- card -->
-          <div class="col-md-4 mt-md-4">
-            <a href="" class="text-decoration-none">
-              <div class="card">
-                <!-- img  card-->
-                <div class="card-Rc">
-                  <img src="./IMG/card-maqueta-2.jpg" class="card-Rc_img img-fluid" alt="">
-                </div>
-                <!-- description -->
-                <div class="card-body">
-                  <!-- title -->
-                  <h4 class="card-title h5 text-dark">
-                    Desarrollo Web
-                  </h4>
-                  <!-- descr -->
-                  <p class="card-text_p">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit dolorem
-                    natus.
-                    <br>
-                    <span class="card-precio">$11,000</span>
-                  </p>
-                  <!-- precio -->
-                  <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
-                </div>
-              </div>
-            </a>
-          </div>
-          <!-- card -->
-          <div class="col-md-4 mt-md-4">
-            <a href="" class="text-decoration-none">
-              <div class="card">
-                <!-- img  card-->
-                <div class="card-Rc">
-                  <img src="./IMG/card-programa-2.jpg" class="card-Rc_img img-fluid" alt="">
-                </div>
-                <!-- description -->
-                <div class="card-body">
-                  <!-- title -->
-                  <h4 class="card-title h5 text-dark">
-                    Base de Datos
-                  </h4>
-                  <!-- descr -->
-                  <p class="card-text_p">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit dolorem
-                    natus.
-                    <br>
-                    <span class="card-precio">$11,000</span>
-                  </p>
-                  <!-- precio -->
-                  <p class="card-txt_cost">$5,000 <span class="card-txt_divi">MXN</span></p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </section>
-    </section>
-  
-        
-
-    
-
-  </body>
-<script src="./js/bootstrap.bundle.min.js"></script>
-</html>
